3771db3a27b9cf6670c2cfe54ca09d53e625ed2f
Chris Briggs <chris.briggs@tfp0labs.com>
2022-12-05 19:56:47 -0500

    Day 5

---
+++ 5.1.py
@@ -0,0 +1,29 @@
+import re
+with open('5.txt') as f:
+    puzzle = [x.strip() for x in f.readlines()]
+    
+instructions = [re.sub('[^0-9]', '', y) for y in puzzle]
+stack = ['HBVWNMLP', 'MQH', 'NDBGFQML', 'ZTFQMWG', 'MTHP', 'CBMJDHGT', 'MNBFVR','PLHMRGS','PDBCN']
+
+for a in instructions:
+    times = int(a[:-2])
+    take_from = int(a[-2]) - 1
+    put_on = int(a[-1]) - 1
+    
+    for b in range(times):
+        stack[put_on] += stack[take_from][-1]
+        stack[take_from] = stack[take_from][:-1]
+
+print([z[-1] for z in stack])
+
+### Input Stack ###
+# [P]     [L]         [T]            
+# [L]     [M] [G]     [G]     [S]    
+# [M]     [Q] [W]     [H] [R] [G]    
+# [N]     [F] [M]     [D] [V] [R] [N]
+# [W]     [G] [Q] [P] [J] [F] [M] [C]
+# [V] [H] [B] [F] [H] [M] [B] [H] [B]
+# [B] [Q] [D] [T] [T] [B] [N] [L] [D]
+# [H] [M] [N] [Z] [M] [C] [M] [P] [P]
+#  1   2   3   4   5   6   7   8   9 
+# stack = ['HBVWNMLP', 'MQH', 'NDBGFQML', 'ZTFQMWG', 'MTHP', 'CBMJDHGT', 'MNBFVR','PLHMRGS','PDBCN']
