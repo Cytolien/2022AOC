304ffccd22a8fbff25843e5a9297efc41c1bd3a4
Chris Briggs <chris.briggs@tfp0labs.com>
2022-12-06 22:09:16 -0500

    Day 6

---
+++ 6.2.py
@@ -0,0 +1,16 @@
+with open('6.txt') as f:
+    puzzle = [x for x in f.readlines()]
+
+count = 0
+temp = []
+answer = []
+for a in puzzle[0]:
+    if count >= 14:
+        if len(set(temp)) == len(temp):
+            answer.append(count)
+        else:
+            temp.pop(0)
+    temp.append(a)    
+    count += 1
+    
+print(min(answer))
